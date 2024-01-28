0d367af870eb84408675a84424f306c0c23c9450
beti <bitasaeedii80@Gmail.com>
2024-01-28 15:05:19 +0330

    info

--- src/components/ServicesInfo.jsx
+++ src/components/ServicesInfo.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import backGround from '../public/serviceBackGround.png';
 import {AllServicesContainer, Service, ServicesContainer, Shadow} from "../styledComponents/ServicesInfo-style";
-
+import { useNavigate } from "react-router-dom";
 function ServicesInfo({t}) {
+    const navigate = useNavigate();
     return (
         <>
             <ServicesContainer image={`url(${backGround})`} id={'blog'}>
@@ -10,25 +11,25 @@ function ServicesInfo({t}) {
                 <h2>{t("servicesInfo1")}</h2>
                 <AllServicesContainer>
 
-                    <Service>
+                    <Service onClick={()=>{ navigate('/price');}}>
                         <h6>{t("serviceTitle1")}</h6>
                         <p>{t("service1")}</p>
                         <a href={''}>{t("servicesInfo3")}</a>
                     </Service>
 
-                    <Service>
+                    <Service onClick={()=>{ navigate('/price');}}>
                         <h6>{t("serviceTitle2")}</h6>
                         <p>{t("service2")}</p>
                         <a href={''}>{t("servicesInfo3")}</a>
                     </Service>
 
-                    <Service>
+                    <Service onClick={()=>{ navigate('/price');}}>
                         <h6>{t("serviceTitle3")}</h6>
                         <p>{t("service3")}</p>
                         <a href={''}>{t("servicesInfo3")}</a>
                     </Service>
 
-                    <Service>
+                    <Service onClick={()=>{ navigate('/price');}}>
                         <h6>{t("serviceTitle4")}</h6>
                         <p>{t("service4")}</p>
                         <a href={''}>{t("servicesInfo3")}</a>
